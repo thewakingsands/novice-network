16b2f2af334b9817cf76b21edd9e02b0f5c4a453
oott123 <git@public.oott123.com>
2019-04-06 00:51:59 +0800

    update

--- docs/.vuepress/toc.js
+++ docs/.vuepress/toc.js
@@ -6,61 +6,113 @@ module.exports = {
         { title: '前言', href: '/' },
         { title: '收费、试玩、福利', href: '/before/pay.html' },
         { title: '世界观和版本', href: '/before/world.html' },
-        { title: '创建角色', href: '#' },
-        { title: '职业和特职', href: '#' }
+        { title: '创建角色', href: '/before/char.html' },
+        { title: '职业和特职', href: '/before/job.html' }
       ]
     },
     {
       title: '基础系统',
       children: [
-        { title: '满级前核心玩法', href: '#' },
-        { title: '任务系统', href: '#' },
-        { title: '装备、装等', href: '#' },
-        { title: '地图、移动', href: '#' },
-        { title: '战斗基础', href: '#' },
-        { title: '常用设置', href: '#' },
-        { title: '直升、回归', href: '#' }
+        { title: '满级前核心玩法', href: '/basic/core.html' },
+        { title: '任务系统', href: '/basic/quest.html' },
+        { title: '装备、装等', href: '/basic/equip.html' },
+        { title: '地图、移动', href: '/basic/map.html' },
+        { title: '战斗基础', href: '/basic/battle.html' },
+        { title: '常用设置', href: '/basic/config.html' },
+        { title: '直升、回归', href: '/basic/reborn.html' }
       ]
     },
     {
-      title: '练级指南',
+      title: '练级路上',
       children: [
-        { title: '练级指南', href: '#' },
-        { title: '装等提升和毕业', href: '#' }
+        { title: '练级指南', href: '/upgrade/guide.html' },
+        { title: '常用系统', href: '/upgrade/basic.html' },
+        { title: '装等提升和毕业', href: '/upgrade/bis.html' }
       ]
     },
     {
       title: '玩法指引',
       children: [
-        { title: '每日/每周内容', href: '#' },
-        { title: '剧情内容', href: '#' },
-        { title: '休闲内容', href: '#' },
-        { title: '生产采集', href: '#' },
-        { title: '休闲战斗', href: '#' },
-        { title: '挑战副本', href: '#' },
-        { title: '发光武器', href: '#' },
-        { title: '发光武器', href: '#' },
-        { title: 'PVP', href: '#' },
-        { title: '深层迷宫（死宫天宫）', href: '#' }
+        { title: '每日/每周内容', href: '/topic/daily.html' },
+        { title: '剧情内容', href: '/topic/story.html' },
+        { title: '休闲内容', href: '/topic/relax.html' },
+        { title: '生产采集', href: '/topic/craft.html' },
+        { title: '休闲战斗', href: '/topic/battle.html' },
+        { title: '挑战副本', href: '/topic/raid.html' },
+        { title: '发光武器', href: '/topic/shine.html' },
+        { title: 'PVP', href: '/topic/pvp.html' },
+        { title: '深层迷宫（死宫天宫）', href: '/topic/dd.html' }
       ]
     },
     {
       title: '职业专题',
       children: [
-        { title: '骑士', href: '#', img: '/images/jobs/paladin.png' },
-        { title: '战士', href: '#', img: '/images/jobs/warrior.png' },
-        { title: '暗黑骑士', href: '#', img: '/images/jobs/darkknight.png' },
-        { title: '武僧', href: '#', img: '/images/jobs/monk.png' },
-        { title: '忍者', href: '#', img: '/images/jobs/ninja.png' },
-        { title: '武士', href: '#', img: '/images/jobs/samurai.png' },
-        { title: '吟游诗人', href: '#', img: '/images/jobs/bard.png' },
-        { title: '机工士', href: '#', img: '/images/jobs/machinist.png' },
-        { title: '黑魔法师', href: '#', img: '/images/jobs/blackmage.png' },
-        { title: '召唤师', href: '#', img: '/images/jobs/summoner.png' },
-        { title: '赤魔法师', href: '#', img: '/images/jobs/redmage.png' },
-        { title: '白魔法师', href: '#', img: '/images/jobs/whitemage.png' },
-        { title: '学者', href: '#', img: '/images/jobs/scholar.png' },
-        { title: '占星术士', href: '#', img: '/images/jobs/astrologian.png' }
+        {
+          title: '骑士',
+          href: '/job/paladin.html',
+          img: '/images/jobs/paladin.png'
+        },
+        {
+          title: '战士',
+          href: '/job/warrior.html',
+          img: '/images/jobs/warrior.png'
+        },
+        {
+          title: '暗黑骑士',
+          href: '/job/darkknight.html',
+          img: '/images/jobs/darkknight.png'
+        },
+        { title: '武僧', href: '/job/monk.html', img: '/images/jobs/monk.png' },
+        {
+          title: '忍者',
+          href: '/job/ninja.html',
+          img: '/images/jobs/ninja.png'
+        },
+        {
+          title: '武士',
+          href: '/job/samurai.html',
+          img: '/images/jobs/samurai.png'
+        },
+        {
+          title: '吟游诗人',
+          href: '/job/bard.html',
+          img: '/images/jobs/bard.png'
+        },
+        {
+          title: '机工士',
+          href: '/job/machinist.html',
+          img: '/images/jobs/machinist.png'
+        },
+        {
+          title: '黑魔法师',
+          href: '/job/blackmage.html',
+          img: '/images/jobs/blackmage.png'
+        },
+        {
+          title: '召唤师',
+          href: '/job/summoner.html',
+          img: '/images/jobs/summoner.png'
+        },
+        {
+          title: '赤魔法师',
+          href: '/job/redmage.html',
+          img: '/images/jobs/redmage.png'
+        },
+        {
+          title: '白魔法师',
+          href: '/job/whitemage.html',
+          img: '/images/jobs/whitemage.png'
+        },
+        {
+          title: '学者',
+          href: '/job/scholar.html',
+          img: '/images/jobs/scholar.png'
+        },
+        {
+          title: '占星术士',
+          href: '/job/astrologian.html',
+          img: '/images/jobs/astrologian.png'
+        }
       ]
     },
     {
