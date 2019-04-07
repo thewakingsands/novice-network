c6f19338f5077458236b873dec28e05ec12b45fc
oott123 <git@public.oott123.com>
2019-04-07 13:01:17 +0800

    fix ci: moving dist dir out

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,23 +1,23 @@
 #!/bin/bash -e
-echo "Preparing deploy"
-ls
-
-echo "Decrypting deploy keys"
 openssl aes-256-cbc -K $encrypted_74088940e92b_key -iv $encrypted_74088940e92b_iv -in ci/deploy-key.enc -out ci/deploy-key -d
+
+### Warning: environment variables will exposed to build logs after this line ###
+set -x
+
 chmod 700 ci
 chmod 600 ci/deploy-key
 
-echo "Starting ssh agent"
 eval `ssh-agent -s`
 ssh-add ci/deploy-key
 
-echo "Init git working directory"
-git -C dist config user.name "bot"
-git -C dist config user.email "root@localhost"
-git -C dist init
-git -C dist remote add origin git@github.com:thewakingsands/novice-network-pages.git
-git -C dist add -A
-git -C dist commit -m "Auto deploy by Travis CI"
+DEPLOY_DIR=$HOME/deploy
+EXEGIT="git -C $DEPLOY_DIR"
 
-echo "Pushing pages"
-git -C dist push -f origin master
+$EXEGIT config user.name "bot"
+$EXEGIT config user.email "root@localhost"
+$EXEGIT init
+$EXEGIT remote add origin git@github.com:thewakingsands/novice-network-pages.git
+$EXEGIT add -A
+$EXEGIT commit -m "Auto deploy by Travis CI"
+
+$EXEGIT push -f origin master
