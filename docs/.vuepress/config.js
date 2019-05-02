14131f52f44a5992767cdc25c7bd8fafc0fd1d4a
oott123 <git@public.oott123.com>
2019-05-02 20:38:58 +0800

    增加一页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ module.exports = {
     '「新大陆见闻录」网站为《最终幻想14》国服新手玩家提供超快超全的指导攻略，是 FF14 玩家居家旅行必备攻略。愿水晶给予你指引。',
   dest: 'dist',
   markdown: {
+    breaks: true,
     anchor: {
       permalink: false
     },
