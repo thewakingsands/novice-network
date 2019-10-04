13fd71f6f8d9a9649ab678599af40b462b093907
oott123 <git@public.oott123.com>
2019-10-04 21:47:07 +0800

    quest component

---
+++ docs/.vuepress/theme/global-components/Quest.vue
@@ -0,0 +1,33 @@
+<template>
+  <span title="点击在最终幻想XIV中文维基上查看详情">
+    <a :href="wikiLink" target="_blank">
+      <img
+        src="https://huiji-public.huijistatic.com/ff14/uploads/2/21/061411.png"
+        alt="任务"
+        class="no-zoom"
+      />{{ name }}</a
+    >
+  </span>
+</template>
+<style lang="stylus" scoped>
+img
+  vertical-align middle
+  cursor pointer!important
+</style>
+<script>
+export default {
+  props: {
+    name: String
+  },
+  computed: {
+    wikiLink() {
+      return (
+        'https://ff14.huijiwiki.com/wiki/' +
+        encodeURIComponent('任务') +
+        ':' +
+        encodeURIComponent(this.name)
+      )
+    }
+  }
+}
+</script>
