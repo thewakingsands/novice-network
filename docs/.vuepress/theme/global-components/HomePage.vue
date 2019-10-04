46ca27b3322e727bc2c7b69a8c8dcfb59599e821
oott123 <git@public.oott123.com>
2019-10-04 21:06:42 +0800

    use routerlink; update desc

--- docs/.vuepress/theme/global-components/HomePage.vue
+++ docs/.vuepress/theme/global-components/HomePage.vue
@@ -2,12 +2,16 @@
   <div class="home-container">
     <div class="mnheader">
       <h1 title="新大陆见闻录">
-        《新大陆见闻录》<img src="/images/title.png" alt="新大陆见闻录" class="no-zoom" />
+        《新大陆见闻录》<img
+          :src="$withBase('/images/title.png')"
+          alt="新大陆见闻录"
+          class="no-zoom"
+        />
       </h1>
       <div class="desc">
         <p>
-          由于《最终幻想14》游戏内容繁多，新人上手也不够友好，新人经常面临迷茫。
-          鉴于此，本站，即《新大陆见闻录》希望能给刚进入《最终幻想14》国服的玩家提供入门级别的攻略指引，帮助新玩家更好地游玩这个游戏。
+          由于《最终幻想14》游戏内容繁多，新人上手也不够友好，新人经常面临不知道做什么好的迷茫。<br />
+          本站<strong>《新大陆见闻录》</strong>希望能给刚进入《最终幻想14》国服的玩家提供入门级别的攻略指引，帮助新玩家更好地游玩这个游戏。
         </p>
         <p>
           新大陆见闻录提供完整的手机布局，确保各位玩家能通过手机、平板访问本站的所有内容。用电脑访问时，工具栏右上角会显示二维码按钮
@@ -15,76 +19,81 @@
           ，鼠标悬浮即可显示当前页面二维码，扫码即可在手机上打开当前页面。
         </p>
         <p>
-          你可以通过<mark class="hide-small">左侧侧边栏</mark><mark class="hide-large">左下角绿色按钮</mark>查看本站的目录。
+          你可以通过<mark class="hide-small">左侧侧边栏</mark
+          ><mark class="hide-large">点击左下角绿色按钮</mark>查看本站的目录。
         </p>
       </div>
     </div>
     <div class="mntab mn-before">
       <h2>入坑准备</h2>
       <ul>
-        <li><a href="before/pay.html">收费方式及福利</a></li>
-        <li><a href="before/world.html">世界观与游戏版本</a></li>
-        <li><a href="before/char.html">下载选服与捏人</a></li>
-        <li><a href="before/job.html">职业选择</a></li>
+        <li><router-link to="before/pay.html">收费方式及福利</router-link></li>
+        <li>
+          <router-link to="before/world.html">世界观与游戏版本</router-link>
+        </li>
+        <li><router-link to="before/char.html">下载选服与捏人</router-link></li>
+        <li><router-link to="before/job.html">职业选择</router-link></li>
       </ul>
     </div>
     <div class="mntab mn-guide">
       <h2>新人指南</h2>
       <ul>
-        <li><a href="basic/core.html">新人必知</a></li>
-        <li><a href="basic/battle.html">战斗基础</a></li>
-        <li><a href="basic/#">练级指南</a></li>
-        <li><a href="basic/config.html">常用设置</a></li>
+        <li><router-link to="basic/core.html">新人必知</router-link></li>
+        <li><router-link to="basic/battle.html">战斗基础</router-link></li>
+        <li><router-link to="basic/#">练级指南</router-link></li>
+        <li><router-link to="basic/config.html">常用设置</router-link></li>
       </ul>
     </div>
     <div class="mntab mn-qna">
       <h2>常见问题</h2>
       <ul>
-        <li><a href="basic/quest.html">推荐任务</a></li>
-        <li><a href="basic/equip.html">装备与幻化</a></li>
-        <li><a href="basic/map.html">地图移动与飞行</a></li>
-        <li><a href="ui/how.html">界面指南</a></li>
+        <li><router-link to="basic/quest.html">推荐任务</router-link></li>
+        <li><router-link to="basic/equip.html">装备与幻化</router-link></li>
+        <li><router-link to="basic/map.html">地图移动与飞行</router-link></li>
+        <li><router-link to="ui/how.html">界面指南</router-link></li>
       </ul>
     </div>
     <div class="mntab mn-sys">
       <h2>进阶内容</h2>
       <ul>
-        <li><a href="#">黑话缩略语词典</a></li>
-        <li><a href="#">副本攻略</a></li>
-        <li><a href="#">货币系统</a></li>
-        <li><a href="#">毕业装</a></li>
+        <li><router-link to="#">黑话缩略语词典</router-link></li>
+        <li><router-link to="#">副本攻略</router-link></li>
+        <li><router-link to="#">货币系统</router-link></li>
+        <li><router-link to="#">毕业装</router-link></li>
       </ul>
     </div>
     <div class="mntab mn-battle">
       <h2>战斗玩法</h2>
       <ul>
-        <li><a href="#">休闲战斗</a></li>
-        <li><a href="#">副本挑战</a></li>
-        <li><a href="#">PVP排位与战场</a></li>
-        <li><a href="#">发光武器</a></li>
-        <li><a href="#">深层迷宫</a></li>
+        <li><router-link to="#">休闲战斗</router-link></li>
+        <li><router-link to="#">副本挑战</router-link></li>
+        <li><router-link to="#">PVP排位与战场</router-link></li>
+        <li><router-link to="#">发光武器</router-link></li>
+        <li><router-link to="#">深层迷宫</router-link></li>
       </ul>
     </div>
     <div class="mntab mn-play">
       <h2>日常休闲</h2>
       <ul>
-        <li><a href="#">每日/每周任务</a></li>
-        <li><a href="#">剧情任务</a></li>
-        <li><a href="#">金碟游乐场</a></li>
-        <li><a href="#">青魔法师</a></li>
-        <li><a href="#">社交活动</a></li>
+        <li><router-link to="#">每日/每周任务</router-link></li>
+        <li><router-link to="#">剧情任务</router-link></li>
+        <li><router-link to="#">金碟游乐场</router-link></li>
+        <li><router-link to="#">青魔法师</router-link></li>
+        <li><router-link to="#">社交活动</router-link></li>
       </ul>
     </div>
     <div class="mntab mn-etc">
       <h2>生产采集</h2>
       <ul>
-        <li><a href="#">能工巧匠（制作）</a></li>
-        <li><a href="#">大地使者（采集）</a></li>
+        <li><router-link to="#">能工巧匠（制作）</router-link></li>
+        <li><router-link to="#">大地使者（采集）</router-link></li>
       </ul>
       <h2>其他</h2>
       <ul>
-        <li><a href="#">周边衍生</a></li>
-        <li><a href="/basic/international.html">国际服专题</a></li>
+        <li><router-link to="#">周边衍生</router-link></li>
+        <li>
+          <router-link to="/basic/international.html">国际服专题</router-link>
+        </li>
       </ul>
     </div>
   </div>
