ef4d79f030fd502e1fabe73b32fa5a93f08a1f43
oott123 <git@public.oott123.com>
2019-10-15 15:54:38 +0800

    施工挂件

---
+++ docs/.vuepress/theme/global-components/UnderConstruction.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ui violet segment ucontainer">
+    <div class="img">
+      <img
+        height="60"
+        src="https://i.loli.net/2019/10/15/nQpFPR478yosaSf.gif"
+      />
+    </div>
+    <div class="text">
+      此处内容仍在施工中，将会在近日有大量内容更新，敬请期待~
+    </div>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+.ucontainer
+  display flex
+  align-items center
+  width 75%
+  min-width 400px
+  margin 0 auto
+  .img
+    width 15%
+    min-width 70px
+    text-align center
+  .text
+    flex 1
+    text-align center
+    font-size 1.3em
+    line-height 1.5
+</style>
