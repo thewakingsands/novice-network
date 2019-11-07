bf59fa2a23d027486c1fbdf543b79cb814ac57d4
hikarievo <hikari.evo@gmail.com>
2019-11-07 11:55:24 +0800

    job update

--- docs/.vuepress/toc.js
+++ docs/.vuepress/toc.js
@@ -177,102 +177,102 @@ module.exports = {
         }
       ]
     },
-    // {
-    //   title: '职业专题',
-    //   children: [
-    //     { title: '职业介绍', href: '/job/' },
-    //     {
-    //       title: '骑士（剑术师）',
-    //       href: '/job/paladin.htm',
-    //       img: '/images/jobs/paladin.png'
-    //     },
-    //     {
-    //       title: '战士（斧术师）',
-    //       href: '/job/warrior.htm',
-    //       img: '/images/jobs/warrior.png'
-    //     },
-    //     {
-    //       title: '暗黑骑士',
-    //       href: '/job/darkknight.htm',
-    //       img: '/images/jobs/darkknight.png'
-    //     },
-    //     {
-    //       title: '绝枪战士',
-    //       href: '/job/gunbreaker.htm',
-    //       img: '/images/jobs/gunbreaker.png'
-    //     },
-    //     {
-    //       title: '武僧（格斗家）',
-    //       href: '/job/monk.htm',
-    //       img: '/images/jobs/monk.png'
-    //     },
-    //     {
-    //       title: '龙骑士（枪术师）',
-    //       href: '/job/dragoon.htm',
-    //       img: '/images/jobs/dragoon.png'
-    //     },
-    //     {
-    //       title: '忍者（双剑师）',
-    //       href: '/job/ninja.htm',
-    //       img: '/images/jobs/ninja.png'
-    //     },
-    //     {
-    //       title: '武士',
-    //       href: '/job/samurai.htm',
-    //       img: '/images/jobs/samurai.png'
-    //     },
-    //     {
-    //       title: '吟游诗人（弓箭手）',
-    //       href: '/job/bard.htm',
-    //       img: '/images/jobs/bard.png'
-    //     },
-    //     {
-    //       title: '机工士',
-    //       href: '/job/machinist.htm',
-    //       img: '/images/jobs/machinist.png'
-    //     },
-    //     {
-    //       title: '舞者',
-    //       href: '/job/dancer.htm',
-    //       img: '/images/jobs/dancer.png'
-    //     },
-    //     {
-    //       title: '黑魔法师（咒术师）',
-    //       href: '/job/blackmage.htm',
-    //       img: '/images/jobs/blackmage.png'
-    //     },
-    //     {
-    //       title: '召唤师（秘术师）',
-    //       href: '/job/summoner.htm',
-    //       img: '/images/jobs/summoner.png'
-    //     },
-    //     {
-    //       title: '赤魔法师',
-    //       href: '/job/redmage.htm',
-    //       img: '/images/jobs/redmage.png'
-    //     },
-    //     {
-    //       title: '青魔法师',
-    //       href: '/job/bluemage.htm',
-    //       img: '/images/jobs/bluemage.png'
-    //     },
-    //     {
-    //       title: '白魔法师（幻术师）',
-    //       href: '/job/whitemage.htm',
-    //       img: '/images/jobs/whitemage.png'
-    //     },
-    //     {
-    //       title: '学者（秘术师）',
-    //       href: '/job/scholar.htm',
-    //       img: '/images/jobs/scholar.png'
-    //     },
-    //     {
-    //       title: '占星术士',
-    //       href: '/job/astrologian.htm',
-    //       img: '/images/jobs/astrologian.png'
-    //     }
-    //   ]
-    // },
+    {
+      title: '职业专题',
+      children: [
+        { title: '职业介绍', href: '/job/' },
+        {
+          title: '骑士（剑术师）',
+          href: '/job/paladin.htm',
+          img: '/images/jobs/paladin.png'
+        },
+        {
+          title: '战士（斧术师）',
+          href: '/job/warrior.htm',
+          img: '/images/jobs/warrior.png'
+        },
+        {
+          title: '暗黑骑士',
+          href: '/job/darkknight.htm',
+          img: '/images/jobs/darkknight.png'
+        },
+        {
+          title: '绝枪战士',
+          href: '/job/gunbreaker.htm',
+          img: '/images/jobs/gunbreaker.png'
+        },
+        {
+          title: '武僧（格斗家）',
+          href: '/job/monk.htm',
+          img: '/images/jobs/monk.png'
+        },
+        {
+          title: '龙骑士（枪术师）',
+          href: '/job/dragoon.htm',
+          img: '/images/jobs/dragoon.png'
+        },
+        {
+          title: '忍者（双剑师）',
+          href: '/job/ninja.htm',
+          img: '/images/jobs/ninja.png'
+        },
+        {
+          title: '武士',
+          href: '/job/samurai.htm',
+          img: '/images/jobs/samurai.png'
+        },
+        {
+          title: '吟游诗人（弓箭手）',
+          href: '/job/bard.htm',
+          img: '/images/jobs/bard.png'
+        },
+        {
+          title: '机工士',
+          href: '/job/machinist.htm',
+          img: '/images/jobs/machinist.png'
+        },
+        {
+          title: '舞者',
+          href: '/job/dancer.htm',
+          img: '/images/jobs/dancer.png'
+        },
+        {
+          title: '黑魔法师（咒术师）',
+          href: '/job/blackmage.htm',
+          img: '/images/jobs/blackmage.png'
+        },
+        {
+          title: '召唤师（秘术师）',
+          href: '/job/summoner.htm',
+          img: '/images/jobs/summoner.png'
+        },
+        {
+          title: '赤魔法师',
+          href: '/job/redmage.htm',
+          img: '/images/jobs/redmage.png'
+        },
+        {
+          title: '青魔法师',
+          href: '/job/bluemage.htm',
+          img: '/images/jobs/bluemage.png'
+        },
+        {
+          title: '白魔法师（幻术师）',
+          href: '/job/whitemage.htm',
+          img: '/images/jobs/whitemage.png'
+        },
+        {
+          title: '学者（秘术师）',
+          href: '/job/scholar.htm',
+          img: '/images/jobs/scholar.png'
+        },
+        {
+          title: '占星术士',
+          href: '/job/astrologian.htm',
+          img: '/images/jobs/astrologian.png'
+        }
+      ]
+    },
     {
       title: '常用网站',
       externalChildren: [
