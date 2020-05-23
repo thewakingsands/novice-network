9d81d69dc4af453a038640122267ea15beac54cb
hikarievo <hikari.evo@gmail.com>
2020-05-23 23:36:36 +0800

    guide update

---
+++ docs/.vuepress/theme/global-components/Role.vue
@@ -0,0 +1,13 @@
+<template>
+  <img
+    :src="$withBase('/images/jobs/' + name + '.png')"
+    class="no-zoom sm-icon" height="20"
+  />
+</template>
+<script>
+export default {
+  props: {
+    name: String
+  }
+}
+</script>
