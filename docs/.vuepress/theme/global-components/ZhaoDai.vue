452f4b05c62246c6657737cbefb03666e7c1d15d
oott123 <git@public.oott123.com>
2020-08-27 22:12:25 +0800

    chore: update zhaodai

--- docs/.vuepress/theme/global-components/ZhaoDai.vue
+++ docs/.vuepress/theme/global-components/ZhaoDai.vue
@@ -3,7 +3,12 @@
 </template>
 
 <script>
-const codeList = ['019w-e5xe-rodk-7344', '018g-0ey9-373r-43s8']
+const codeList = [
+  '019w-e5xe-rodk-7344', // yz
+  '018g-0ey9-373r-43s8', // xys
+  '056h-k2dq-xbgl-55vj', // fs
+  '018t-uj1a-hr7j-5318' // ab
+]
 
 export default {
   data() {
